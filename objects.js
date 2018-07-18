538a80f37f7851ff30f09f0688b8769d6d6240b6
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 13:28:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,7 +10,9 @@ function updatePlaylist(playlist, artistName, songTitle){
 
 updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
-
+function removeFromPlaylist(playlist, artistName){
+  delete 
+}
 
 /* if i want to add to a playlist non-destructively i can use the below code - tested & works in chrome web brower console
 
